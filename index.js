0bd6d4734d3d5d174ddc85e38c20e03aa157220c
nawfalrakhmansah <nrakhmansah@gmail.com>
2026-02-02 16:42:12 +0700

    [200~minimal website for deploy

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
 const express = require("express");
-const path = require("path");
 const app = express();
+const PORT = 3000;
 
-// static file (gambar, css)
 app.use(express.static("public"));
 
-// homepage
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "index.html"));
+  res.send("Info Football - Deploy Success 🚀");
 });
 
-// halaman club
-app.get("/club/:name", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "club.html"));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
-
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
-
